aa9e7de8cdd21b90ba1194dd78f071e0d1a8368a
Michael Chinye <michaelchinyechinye2018@gmail.com>
2022-05-22 03:59:49 +0800

    completed implimentationof new changes

--- Js/carousel.js
+++ Js/carousel.js
@@ -5,18 +5,12 @@ function matchLargeScreen(x) {
           showSlides(slideIndex);
      }
    }
-   function matchSmallScreen(x) {
-     if (x.matches) { // If media query matches
-          showSecondSlides(slideIndex);
-     }
-   }
+
 let smallDevice = window.matchMedia("(max-width: 767px)");
 let largeDevice = window.matchMedia("(min-width: 912px) and (max-width: 1500px)");
 largeDevice.addEventListener('change', matchLargeScreen);
-smallDevice.addEventListener('change', matchSmallScreen);
 document.addEventListener('DOMContentLoaded', function(){
      matchLargeScreen(largeDevice) // Call listener function at run time
-     matchSmallScreen(smallDevice);
 });
 function showSlides(n) {
      let i;
@@ -33,26 +27,26 @@ function showSlides(n) {
      slides[slideIndex-1].style.display = "flex";
      //dots[slideIndex-1].className += " active";
 }
-function showSecondSlides(n) {
-     let i;
-     let slides = document.getElementsByClassName("mentor");
-     let dots = document.getElementsByClassName("dot");
-     if (n > slides.length) {slideIndex = 1}
-     if (n < 1) {slideIndex = slides.length}
-     for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-     }
-     for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "" );
-     }
-     slides[slideIndex-1].style.display = "flex";
-     //dots[slideIndex-1].className += " active";
-}
-// Next/previous controls
-function plusSlides(n) {
-     showSlides(slideIndex += n);
+// function showSecondSlides(n) {
+//      let i;
+//      let slides = document.getElementsByClassName("mentor");
+//      let dots = document.getElementsByClassName("dot");
+//      if (n > slides.length) {slideIndex = 1}
+//      if (n < 1) {slideIndex = slides.length}
+//      for (i = 0; i < slides.length; i++) {
+//           slides[i].style.display = "none";
+//      }
+//      for (i = 0; i < dots.length; i++) {
+//           dots[i].className = dots[i].className.replace(" active", "" );
+//      }
+//      slides[slideIndex-1].style.display = "flex";
+//      //dots[slideIndex-1].className += " active";
+// }
+// // Next/previous controls
+// function plusSlides(n) {
+//      showSlides(slideIndex += n);
 
-   }
+//    }
 function slideMentorsGallery(n){
      showSecondSlides(slideIndex +=n);
 }
--- Js/index.js
+++ Js/index.js
@@ -1,4 +1,3 @@
-
 $(window).on('load', function(e){
      let floatOffSet = $('.float-btn').offset().top;
      $(window).scroll(function(){
@@ -14,4 +13,40 @@ $(window).on('load', function(e){
                });
           }
      });
-});
+     // $(document).ready(function myFunction(){
+     //      if (x.matches) { // If media query matches
+     //           document.getElementById('demo').carousel('dispose')
+     //           console.log('it matches')
+     //      } else {
+     //           console.log('it does not match')
+     //      }
+     // })
+     function myFunction(x) {
+          if (x.matches) { // If media query matches
+               $('#demo').carousel('dispose')
+               document.getElementById('demo').removeAttribute('data-ride')
+               document.getElementById('demo').classList.remove('carousel')
+               document.getElementById('carouselPrev'). removeAttribute('data-slide')
+               document.getElementById('carouselNext'). removeAttribute('data-slide')
+               const mentors = document.getElementsByClassName('mentor');
+               
+               for (var i = 0; i < mentors.length; i++) {
+                    mentors[i].removeAttribute('data-aos')
+               }
+   
+               // console.log('it matches')
+          } else {
+               // console.log('it does not match')
+          }
+     }
+        
+        var x = window.matchMedia("(max-width: 767px)")
+        myFunction(x) // Call listener function at run time
+        x.addEventListener('change', myFunction) // Attach listener function on state changes
+
+}
+
+
+
+
+);
